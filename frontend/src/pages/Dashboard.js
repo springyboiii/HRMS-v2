0f4635e5453f4a117b4d9d93f6b414d9e14bfd47
zafrarifky <zafra.19@cse.mrt.ac.lk>
2022-06-20 16:47:44 +0530

    Dashboard still in progress

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -5,93 +5,88 @@ import Footer from '../components/Footer';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../components/Navbar.css';
+import Header from '../components/Header';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Dashboard = () => {
     const goToPersonal = () => {
-//redirect to personal info page
+        //redirect to personal info page
     }
 
 
     return (
         <div>
-            {[false].map((expand) => (
-                <Navbar key={expand} bg="dark" variant="dark" expand={expand} className="mb-3">
-                    <Container fluid>
-                       
-                        <Nav className="mx-auto">
-                            <Container fluid>
-                                <Navbar.Brand href="#home">
-                                    <img
-                                        src="/logo3.png"
-                                        width="30"
-                                        height="30"
-                                        className="d-inline-block align-top"
-                                        alt="React Bootstrap logo"
-                                    />
-                                </Navbar.Brand>
-                                <Navbar.Brand href="#">Employee Management System</Navbar.Brand>
-                            </Container>
-                        </Nav>
-                    </Container>
-                </Navbar>
-            ))}
+            <Header />
 
             <div className='text-center'><h1>Dashboard</h1></div>
-            <div className='row container'>
-                <div className='col-md-3 container'>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="slide1.jpeg" />
-                        <Card.Body>
-                            <Card.Title>View Personal Information</Card.Title>
-                            <Card.Text>
-                                View your personal information at a glance.
-                            </Card.Text>
-                            <Button variant="primary" onClick={goToPersonal()}>Personal Info</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
 
-                <div className='col-md-3 container'>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="slide2.jpeg" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                
-                <div className='col-md-3 container'>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="slide3.jpg" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className='col-md-3 container'>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="slide4.jpg" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-            </div>
+            <Container>
+                <Carousel>
+                    <Carousel.Item>
+                        <div className='row container'>
+                            <div className='col-md-4 container'>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="slide1.jpeg" />
+                                    <Card.Body>
+                                        <Card.Title>View Personal Information</Card.Title>
+                                        <Card.Text>
+                                            View your personal information at a glance.
+                                        </Card.Text>
+                                        <Button variant="primary" onClick={goToPersonal()}>Personal Info</Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+
+                            <div className='col-md-4 container'>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="slide2.jpeg" />
+                                    <Card.Body>
+                                        <Card.Title>Apply for Leave</Card.Title>
+                                        <Card.Text>
+                                            Apply for a leave.
+                                        </Card.Text>
+                                        <Button variant="primary" >Leave Application</Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+
+                            <div className='col-md-4 container'>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="slide4.jpg" />
+                                    <Card.Body>
+                                        <Card.Title>View Leave Details</Card.Title>
+                                        <Card.Text>
+                                            Take a look at previous leaves you have taken.
+                                        </Card.Text>
+                                        <Button variant="primary" >View Leaves</Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className='row container'>
+                            <div className='col-md-4 container'>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="slide4.jpg" />
+                                    <Card.Body>
+                                        <Card.Title>Change your Password</Card.Title>
+                                        <Card.Text>
+                                            Change your password to a stronger private one.
+                                        </Card.Text>
+                                        <Button variant="primary" disabled>Change Password</Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+
+                    </Carousel.Item>
+                </Carousel>
+            </Container>
+
+
             <Footer />
         </div>
     )
