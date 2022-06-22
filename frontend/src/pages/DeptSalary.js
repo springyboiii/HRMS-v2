c824041d24e573a6bee529a7f75ff2e39150deac
gnishaanthini <nishaanthini.19@cse.mrt.ac.lk>
2022-06-22 17:06:47 +0530

    subsubmenu changes

---
+++ frontend/src/pages/DeptSalary.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Footer from '../components/Footer';
+
+import Header from '../components/Header';
+
+
+const DeptSalary = () => {
+  
+  return (
+
+    <div>
+      <Header />
+      <h1>Hello </h1>
+
+
+      <Footer />
+    </div>
+  )
+}
+
+export default DeptSalary
---
+++ frontend/src/pages/PaySalary.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Footer from '../components/Footer';
+
+import Header from '../components/Header';
+
+
+const PaySalary = () => {
+  
+  return (
+
+    <div>
+      <Header />
+      <h1>Hello </h1>
+
+
+      <Footer />
+    </div>
+  )
+}
+
+export default PaySalary
